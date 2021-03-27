a894476956905fd7a8e83e63134d7a00751ca408
stuti-v4 <stutiverma48@gmail.com>
2021-03-28 01:05:19 +0530

    Added Component
    Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { UseraddComponent } from './useradd/useradd.component';
+import { ShowuserComponent } from './showuser/showuser.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, UseraddComponent, ShowuserComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { 
