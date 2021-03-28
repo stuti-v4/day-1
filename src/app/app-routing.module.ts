76b160d0c291f092ec1a99de4c2b2ee3dba4705a
stuti-v4 <stutiverma48@gmail.com>
2021-03-28 17:20:10 +0530

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ShowuserComponent } from './showuser/showuser.component';
 import { UseraddComponent } from './useradd/useradd.component';
 
 const routes: Routes=[
@@ -11,6 +12,10 @@ const routes: Routes=[
   {
     path:'useradd',
     component:UseraddComponent
+  },
+  {
+    path:'showuser',
+    component:ShowuserComponent
   }
 ];
 
@@ -20,4 +25,4 @@ const routes: Routes=[
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeComponent,UseraddComponent,];
+export const routingComponents = [HomeComponent,UseraddComponent,ShowuserComponent];
