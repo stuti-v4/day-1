b6931f05a923eb9a26db9cd562b573b44138ebe5
stuti-v4 <stutiverma48@gmail.com>
2021-04-03 12:44:45 +0530

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CovidmeterComponent } from './covidmeter/covidmeter.component';
 import { HomeComponent } from './home/home.component';
 import { ShowuserComponent } from './showuser/showuser.component';
 import { UseraddComponent } from './useradd/useradd.component';
@@ -14,6 +15,10 @@ const routes: Routes=[
   {
     path:'useradd',
     component:UseraddComponent
+  },
+  {
+    path:'covidmeter',
+    component:CovidmeterComponent
   },
   {
     path:'showuser',
